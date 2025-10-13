2d87c687d438e8c2f1bd27fdf58e09991edd3353
huy pham <huypham@umich.edu>
2025-10-13 16:53:39 -0400

    Read Function

---
+++ Project_1.py
@@ -0,0 +1,48 @@
+# SI 201 Project 1
+# Your name: Huy Pham
+# Your student id: 3483 3492
+# Your email: huypham@umich.edu
+# Who or what you worked with on this homework (including generative AI like ChatGPT):
+# Felicia Wang, John (Yohan) Park
+
+import os
+import unittest
+import csv
+
+# INPUT: file_path (str): The path to the penguins.csv file.
+# OUTPUT: list of dictionaries, where each dictionary represents a row in the CSV file.
+# This function reads the penguins.csv file and returns a list of dictionaries
+# where each dictionary represents a row in the CSV file.
+# Including: Species, Island, Bill Length (mm), Bill Depth (mm), 
+#            Flipper Length (mm), Body Mass (g), Sex, and Year.
+def read_penguins_csv(file_path):
+    
+    # Initialize an empty list to store the dictionaries.
+    list_of_dicts = []
+    
+    # Open the CSV file and read its contents.
+    inFile = open(file_path, 'r')
+    penguins = csv.reader(inFile)
+    
+    # Skip the header row.
+    header = next(penguins)
+    
+    # Loops through each row.
+    for row in penguins:
+        # Initialize an empty dictionary for each row.
+        penguin_dict = {}
+        # Loops through each column in the row, creating a key and value pair
+        for i in range(len(header)):
+            penguin_dict[header[i]] = row[i]
+        # Adds the dictionary to the list.
+        list_of_dicts.append(penguin_dict)  
+            
+    # Close the file.
+    inFile.close()  
+    
+    return list_of_dicts
+
+def main():
+
+    if __name__ == '__main__':
+        main()
